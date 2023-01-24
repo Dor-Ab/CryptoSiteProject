a1c2f58d02b1777a7b9df56c7bb253ae184942d8
Dor Abutbul <Doronabutbul@gmail.com>
2023-01-24 18:14:26 +0200

    Added show only favorites button

--- main.js
+++ main.js
@@ -219,6 +219,7 @@ $(() => {
         for (let card of cards) {
             const coinName = $(card).children().children().children(".card-title").text()
             const coinSymbol = $(card).children().children().children(".card-text").text()
+
             if (!coinName.includes(inputCoins) && !coinName.toLowerCase().includes(inputCoins) && !coinSymbol.includes(inputCoins)) {
                 hideCards.push(card)
                 $(hideCards).hide()
@@ -293,6 +294,69 @@ $(() => {
             // console.log(favoriteCoins)
         })
     }
+
+    // Show Favorites Only
+    $("#favoriteCardsBtn").on("click", function () {
+
+        if (favoriteCoins.length === 0) {
+            $(".card").parent(".col").fadeIn(200)
+            const showEye = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye"
+            viewBox="0 0 16 16">
+            <path
+                d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+        </svg>`
+
+            $(this).children("span").text("Show Favorites")
+            $(this).children("svg").html(showEye)
+        }
+
+
+        if (favoriteCoins.length >= 1) {
+            let hideOrShowText = $(this).children("span").text()
+            const cards = $(".card").parent(".col")
+
+            let showCards = []
+
+            if (hideOrShowText === "Show Favorites") {
+
+                for (let card of cards) {
+                    for (let fav of favoriteCoins) {
+                        if (fav.id === $(card).children(".card").children().attr("data-coinId")) {
+                            showCards.push(card)
+                        }
+                    }
+                }
+
+                const hideEye = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash" viewBox="0 0 16 16">
+                <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
+                <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
+                <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
+              </svg>`
+
+                cards.fadeOut(200)
+                $(showCards).fadeIn(200)
+                $(this).children("span").text("Hide Favorites")
+                $(this).children("svg").html(hideEye)
+            }
+
+            else if (hideOrShowText === "Hide Favorites") {
+                const showEye = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye"
+                viewBox="0 0 16 16">
+                <path
+                    d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+            </svg>`
+
+                cards.fadeIn(200)
+                $(this).children("span").text("Show Favorites")
+                $(this).children("svg").html(showEye)
+            }
+        }
+
+
+
+    })
 
     // Opens Modal If More Then 5 Coins Selected
     function OpenBootstrapModal(coinSymbol) {
