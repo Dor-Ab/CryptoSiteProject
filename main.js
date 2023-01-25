e613297515e8f5b386df0bec84aa9fcba3729ec6
Dor Abutbul <Doronabutbul@gmail.com>
2023-01-25 15:13:21 +0200

    Fixed bug on show faves button

--- main.js
+++ main.js
@@ -307,7 +307,7 @@ $(() => {
             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
         </svg>`
 
-            $(this).children("span").text("Show Favorites")
+            $(this).children("span").text("Show Faves")
             $(this).children("svg").html(showEye)
         }
 
@@ -318,7 +318,7 @@ $(() => {
 
             let showCards = []
 
-            if (hideOrShowText === "Show Favorites") {
+            if (hideOrShowText === "Show Faves") {
 
                 for (let card of cards) {
                     for (let fav of favoriteCoins) {
@@ -336,11 +336,11 @@ $(() => {
 
                 cards.fadeOut(200)
                 $(showCards).fadeIn(200)
-                $(this).children("span").text("Hide Favorites")
+                $(this).children("span").text("Hide Faves")
                 $(this).children("svg").html(hideEye)
             }
 
-            else if (hideOrShowText === "Hide Favorites") {
+            else if (hideOrShowText === "Hide Faves") {
                 const showEye = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye"
                 viewBox="0 0 16 16">
                 <path
@@ -349,7 +349,7 @@ $(() => {
             </svg>`
 
                 cards.fadeIn(200)
-                $(this).children("span").text("Show Favorites")
+                $(this).children("span").text("Show Faves")
                 $(this).children("svg").html(showEye)
             }
         }
